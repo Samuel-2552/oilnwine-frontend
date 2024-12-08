ac8359240d92f1df5ec73e908a14cbb1a2bdb31a
Samuel-2552 <201501502@rajalakshmi.edu.in>
2024-12-08 18:56:04 +0530

    updated routers

--- app/login/page.js
+++ app/login/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,8 @@
+"use client";
+
 import { useAuth } from "../hooks/useAuth";
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
   const { login } = useAuth();
